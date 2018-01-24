7bd9b4fbdfc6c6fa910aad810c112aebd72501f7
Roman Fedorov <rausdi@gmail.com>
2018-01-24 18:53:50 +0300

    Added post method to save form data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const sportsmen = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Add a sportsman',
+      sportsmen: sportsmen
+  });
+});
+
+router.post('/', function(req, res) {
+  const sportsman = req.body;
+  sportsmen.push({
+      ID: Math.random().toString(36).substr(2, 9),
+      lastName: sportsman.lastName,
+      firstName: sportsman.firstName,
+      sportType: sportsman.sportType
+  });
+  res.render('index', {
+    title: 'Data added',
+    sportsmen: sportsmen
+  })
 });
 
 module.exports = router;
