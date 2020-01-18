e8e921317601587781e4a6355ee4337b8cc90b08
Roman Fedorov <rausdi@gmail.com>
2020-01-18 21:33:56 +0300

    change port

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Headers', 'authorization,content-type');
     next();
 });
